e09c2c06002423a98eeca310a77ec4c88d283f41
Omkar J Saraf <omkarjsaraf@gmail.com>
2018-07-22 21:49:08 -0400

    First App project
    This project is the first project, for understanding and experimenting with ionic framework for creating an App. The idea of the project is to create a Photo gallery, hence the name Pho(photo)galle(gallery).

---
+++ src/pages/flower/flower.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-flower',
+  templateUrl: 'flower.html'
+})
+export class FlowerPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
